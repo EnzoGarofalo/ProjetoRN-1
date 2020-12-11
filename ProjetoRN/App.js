f0bdee2aa54e24a7e8bc9eb0c63ff9f402f2eb33
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-11 12:31:57 -0300

    Update App.js

--- ProjetoRN/App.js
+++ ProjetoRN/App.js
@@ -5,6 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+//Dependencias de Screens
+import inicial from './src/views/inicial/inicial.js';
+import item from './src/views/item/item.js';
+import perfil from './src/views/perfil/Perfil.js';
+import consultarPerfil from './src/views/perfil/consultaPerfil.js';
+import criarPerfil from './src/views/perfil/criarPerfil.js';
 
 // Fornece um caminho para o seu app transitar entre telas, cada nova tela é colocada no topo da pilha
 const Stack = createStackNavigator();
@@ -14,9 +20,11 @@ export default function App() {
       //É o responsavel por manipular o estado do app e conectar o 'navigator' com o ambiente do app:
     <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Inicial" component={inicial}/>
-                <Stack.Screen name="Item" component={item}/>
-                <Stack.Screen name="Perfil" component={perfil}/>
+                <Stack.Screen name="inicial" component={inicial}/>
+                <Stack.Screen name="item" component={item}/>
+                <Stack.Screen name="perfil" component={perfil}/>
+                <Stack.Screen name="consultar Perfil" component={consultarPerfil}/>
+                <Stack.Screen name="criar Perfil" component={criarPerfil}/>
             </Stack.Navigator>
     </NavigationContainer>
     );
