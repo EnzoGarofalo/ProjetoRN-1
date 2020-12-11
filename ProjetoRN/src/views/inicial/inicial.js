9d209756842350b2748d6b000142c3c0a78d415f
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-11 13:41:03 -0300

    update

---
+++ ProjetoRN/src/views/inicial/inicial.js
@@ -0,0 +1,22 @@
+// useState é um HOOK, recurso do RN, no caso é usado para preservar valores de qlqr tipo, sem  classes
+import React, { useState } from 'react'; 
+import estiloInicial from './estiloInicial';
+import { Text, View, TouchableOpacity} from 'react-native';
+
+function inicial ({ navigation }){
+    return(
+        
+        const abrirPerfil = ( ) => {
+            navigation.navigate('perfil');
+        }
+
+    )
+}
+
+
+
+
+
+
+export default inicial;
+
