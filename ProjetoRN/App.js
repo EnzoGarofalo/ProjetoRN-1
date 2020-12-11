3f8ed0fcb23dd349590cb6fc7799e73c38b9724f
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-11 12:18:38 -0300

    update

--- ProjetoRN/App.js
+++ ProjetoRN/App.js
@@ -5,12 +5,19 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+
+// Fornece um caminho para o seu app transitar entre telas, cada nova tela é colocada no topo da pilha
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-      //
+      //É o responsavel por manipular o estado do app e conectar o 'navigator' com o ambiente do app:
     <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Inicial" component={inicial}/>
+                <Stack.Screen name="Item" component={item}/>
+                <Stack.Screen name="Perfil" component={perfil}/>
+            </Stack.Navigator>
     </NavigationContainer>
     );
 }
