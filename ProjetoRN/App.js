d06394d9e2fc753bfdbdb1c2affc0f085d3f5610
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-11 09:22:24 -0300

    Update App.js

--- ProjetoRN/App.js
+++ ProjetoRN/App.js
@@ -1,9 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+// dependencias de navegação
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+
 
 export default function App() {
   return (
