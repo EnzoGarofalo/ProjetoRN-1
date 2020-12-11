39c50786aec60c9b764b2dd2f0da522fcdfc7e33
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-11 09:23:24 -0300

    Update App.js

--- ProjetoRN/App.js
+++ ProjetoRN/App.js
@@ -6,8 +6,6 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
-
 export default function App() {
   return (
     <View style={styles.container}>
