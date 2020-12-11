142b6d4cea86fed28daa6a93c53f788b0d75ef6a
Enzo Garofalo <enzo.garofalo07@gmail,com>
2020-12-11 15:51:59 -0300

    update

--- ProjetoRN/src/views/item/item.js
+++ ProjetoRN/src/views/item/item.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FlatList } from 'react-native';
 import estiloItem from './estiloItem';
 import { Text, View, TouchableOpacity}  from 'react-native';
+import Lista from '../../lista/lista';
 
 function item ({ navigation }){
 
@@ -17,9 +18,16 @@ function item ({ navigation }){
     const voltar = () => {
         navigation.goBack();
     }
+<View style={estiloItem.container}>
 
+    <FlatList 
+        showsHorizontalScrollIndicator={false}
+        keyExtractor= {(item) => item.id}
+        data={item}
+        renderItem= {({item}) => <Lista data={item}/>}
+    />
     
-
+</View>
 }
 
 export default item;
